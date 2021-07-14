8c027d1c5b78280c48a05b7eaa8998dc151a75be
Godlove <ntangakalegodlove@gmail.com>
2021-07-14 02:39:07 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,2 @@
 var date = document.querySelector('.year');
-var d = new Date();
-var year = d.getFullYear();
-var h = d.getHours();
-var m = d.getMinutes();
-var mon = d.getMonth();
-var day = d.getUTCDate();
-date.innerHTML = `The Time  Today is  ${h}:${m} in the ${mon}th month of the current Year.<br> 2018 - ${year}`;
+date.innerHTML = new Date().toLocaleDateString('en-us');
